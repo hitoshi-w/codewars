de9a4441fd1fcdb6d3cc3b8d4d71fa9e1bcdbf70
hitoshi-w <wh.01.11.777@gmail.com>
2019-11-26 21:57:42 +0900

    Update the_observed_pin

--- 4kyu/the_observed_pin.rb
+++ 4kyu/the_observed_pin.rb
@@ -37,3 +37,19 @@ def get_pins(input)
   for index in 1..input_len - 1 do
     r = r.product(input_arr[index])
   end
+
+  #best practice
+  # def get_pins(observed)
+  # mapping = {
+  #   "1" => %w(1 2 4),
+  #   "2" => %w(1 2 3 5),
+  #   "3" => %w(2 3 6),
+  #   "4" => %w(1 4 5 7),
+  #   "5" => %w(2 4 5 6 8),
+  #   "6" => %w(3 5 6 9),
+  #   "7" => %w(4 7 8),
+  #   "8" => %w(5 7 8 9 0),
+  #   "9" => %w(6 8 9),
+  #   "0" => %w(8 0)
+  # }
+  # observed.split(//).map {|key| mapping[key] }.reduce {|m, x| m.product(x).map(&:join) }
